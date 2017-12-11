8d3da2eb720c5a469f78a254c0b04a2442cc61fb
Pusinfo-013 <Pusinfo-013@Pusinfowas-013>
2017-12-11 14:22:59 +0700

    Perubahan 11.12.17

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,5 +1,12 @@
 package hello;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+//import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
@@ -28,6 +35,28 @@ public class GreetingController {
     	data.add("Brunei");
     	data.add("Timor Leste");
     	
-    	return data.stream().filter(line->line.contains(prefix)).collect(Collectors.toList());
+    	return data.stream().filter(line->line.startsWith(prefix)).collect(Collectors.toList());
+    }
+    
+    @RequestMapping("/countries")
+    public String getCountries() throws IOException{
+    URL url=new URL("http://www.webservicex.net/globalweather.asmx/GetCitiesByCountry");
+    URLConnection connection=url.openConnection();
+   // connection.setDoOutput(true);
+    connection.setRequestProperty("Content-Type", "aplication/x-www-form-urlencoded");
+    connection.setRequestProperty("Content-Length", "0");
+    
+    InputStream stream=url.openConnection().getInputStream(); //stream adalah aliran data
+    InputStreamReader reader=new InputStreamReader(stream); //untuk membaca aliran data stream
+    BufferedReader buffer=new BufferedReader(reader);
+    
+    String line;
+    StringBuilder builder= new StringBuilder();
+    while ((line=buffer.readLine()) !=null) {
+    	builder.append(line);
+    }
+	return builder.toString();
+    
+    //return new BufferedReader(InputStreamReader(stream)).lines().collect(collector.joining("\n"));
     }
 }
