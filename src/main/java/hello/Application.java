096024fb0bf59c5245c2a73489aee323b7290ddc
Pusinfo-013 <Pusinfo-013@Pusinfowas-013>
2017-12-11 17:12:04 +0700

    perubahan jam 5 sore 11.12.17

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -2,6 +2,7 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -9,4 +10,10 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+    
+    @Bean("abc")
+    public Greeting sample() {
+    	return new Greeting(18,"BPKP");    	
+    }
+    
 }
